8fd8a5c0a4b3cd7de82211bdb985801e743e3694
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-02-08 22:19:09 -0800

    add remove button on hover

--- memegen.js
+++ memegen.js
@@ -2,12 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const memeRoll = document.getElementById('meme-roll');
 
   const formBlock = document.querySelector("form");
+
+  /* -------------------------------------------------------------------------- */
+  /*                         Submit form Event Listener                         */
+  /* -------------------------------------------------------------------------- */
+
   formBlock.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const imageURL = document.querySelector('#image-url');
     const topText = document.querySelector('#top-text');
     const bottomText = document.querySelector('#bottom-text');
+
+    /* -------------------------------------------------------------------------- */
+    /*                  Add Random Cat Image if Image Field Blank                 */
+    /* -------------------------------------------------------------------------- */
 
     if (!imageURL.value) {
       const randomCats = [
@@ -20,7 +29,12 @@ document.addEventListener('DOMContentLoaded', function() {
       imageURL.value = randomCats[Math.floor(Math.random() * randomCats.length)];
     }
 
+    /* -------------------------------------------------------------------------- */
+    /*                        Add Random Text if Text Blank                       */
+    /* -------------------------------------------------------------------------- */
+
     if (!topText.value && !bottomText.value) {
+
       const randomTopText = [
         "I don't think that memes",
         "I am silently",
@@ -28,6 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "Drop",
         "What if I told you"
       ]
+
       const randomBottomText = [
         "What you think it memes",
         "Correcting your grammar",
@@ -35,17 +50,22 @@ document.addEventListener('DOMContentLoaded', function() {
         "The bass",
         "'The Cloud' is just someone else's computer"
       ]
+
       const rand = Math.floor(Math.random() * randomTopText.length)
+
       topText.value = randomTopText[rand];
       bottomText.value = randomBottomText[rand];
     }
 
-    addMeme(imageURL, topText, bottomText);
-
-    imageURL.value = "";
-    topText.value = "";
-    bottomText.value = "";
+    const newMeme = addMeme(imageURL, topText, bottomText);
+    addOverlay(newMeme);
+    clearForm(imageURL, topText, bottomText);
   });
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                        Add Meme to Meme Roll Section                       */
+  /* -------------------------------------------------------------------------- */
 
   function addMeme(imageURL, topText, bottomText) {
     const memeContainer = document.createElement('div');
@@ -61,7 +81,6 @@ document.addEventListener('DOMContentLoaded', function() {
     memeContainer.appendChild(topLine);
     memeContainer.appendChild(bottomLine);
 
-
     topLine.classList.add('top-line');
     topLine.innerText = topText.value.toUpperCase();
 
@@ -69,5 +88,58 @@ document.addEventListener('DOMContentLoaded', function() {
     bottomLine.innerText = bottomText.value.toUpperCase();
     memeRoll.appendChild(memeContainer);
 
+    return memeContainer;
+
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                              Add Close Overlay                             */
+  /* -------------------------------------------------------------------------- */
+
+  function addOverlay(meme) {
+    const overlayDiv = document.createElement("div");
+    overlayDiv.style.backgroundColor = "rgba(0,0,0,0.5)";
+    overlayDiv.style.position = "absolute";
+    overlayDiv.style.top = "0";
+    overlayDiv.style.left = "0";
+    overlayDiv.style.width = "100%";
+    overlayDiv.style.height = "100%";
+    overlayDiv.style.display = "none";
+    overlayDiv.style.borderRadius = "5px";
+    overlayDiv.style.justifyContent = "space-around";
+    overlayDiv.style.alignItems = "center";
+    overlayDiv.classList.add = "remove-meme"
+
+    const closeIcon = document.createElement('img');
+    closeIcon.src = 'assets/close.png';
+    closeIcon.style.width = '50px';
+    closeIcon.style.height = '50px';
+    overlayDiv.appendChild(closeIcon);
+
+    meme.appendChild(overlayDiv);
+    meme.addEventListener("mouseenter", function(event) {
+      event.target.lastElementChild.style.display = "flex";
+    });
+
+    meme.addEventListener("mouseleave", function(event) {
+      event.target.lastElementChild.style.display = "none";
+    });
+
+    const memeRemovalDiv = meme.lastElementChild;
+
+    memeRemovalDiv.addEventListener("click", function(event) {
+      event.target.parentElement.remove();
+    })
+
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                         Clear the form after submit                        */
+  /* -------------------------------------------------------------------------- */
+
+  function clearForm(imageURL, topText, bottomText) {
+    imageURL.value = "";
+    topText.value = "";
+    bottomText.value = "";
   }
 });
